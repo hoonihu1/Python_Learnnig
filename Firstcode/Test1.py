6a95054f064572cd1da0a7fc68c0799425e9f016
hoonihu1 <hoonihu@gmail.com>
2019-08-08 14:28:30 +0900

    190808_python

---
+++ Firstcode/Test1.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+t.color("red")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+t.color("green")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
